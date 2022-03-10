2918ce746d833ab9a7c7e2e11a19731fe4eb574d
Rafael Lunardelli <rafael@devsquad.com>
2022-03-10 16:34:41 -0700

    installing php code sniffer

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller
+{
+    public function get_user()
+    {
+        $variable_with_underscore = 1;
+        
+        return [];
+    }
+}
