c0036538c05443307ad39298046e758a9b7d7a6b
Rafael Lunardelli <rafael@devsquad.com>
2022-03-10 16:40:00 -0700

    installing phpstan

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,4 @@ namespace App\Http\Controllers;
 
 class UserController extends Controller
 {
-    public function get_user()
-    {
-        $variable_with_underscore = 1;
-        
-        return [];
-    }
 }
