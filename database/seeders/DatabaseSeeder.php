47a7a2a386abe3cb2f0a4587a08332462f2300ec
Rafael Lunardelli <rafael@devsquad.com>
2022-04-07 16:40:07 -0600

    T-7: wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name'  => 'Rafael Lunardelli',
+            'email' => 'pinguim@dolaravel.com'
+        ]);
     }
 }
