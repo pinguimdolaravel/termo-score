1bb7b8f7fa93de2202b8b3f4552ed3e5a21fe49c
Rafael Lunardelli <rafael@devsquad.com>
2022-06-30 18:24:46 -0600

    TS-10: wip

--- app/Policies/GroupPolicy.php
+++ app/Policies/GroupPolicy.php
@@ -10,19 +10,6 @@ class GroupPolicy
 {
     use HandlesAuthorization;
 
-    public function viewAny(User $user): bool
-    {
-
-    }
-
-    public function view(User $user, Group $group): bool
-    {
-    }
-
-    public function create(User $user): bool
-    {
-    }
-
     public function update(User $user, Group $group): bool
     {
         return $user->is($group->user);
@@ -31,13 +18,5 @@ class GroupPolicy
     public function delete(User $user, Group $group): bool
     {
         return $user->is($group->user);
-    }
-
-    public function restore(User $user, Group $group): bool
-    {
-    }
-
-    public function forceDelete(User $user, Group $group): bool
-    {
     }
 }
