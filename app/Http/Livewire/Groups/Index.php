ade972a937239032012acaa03a62e7c8c26e9e5c
Rafael Lunardelli <rafael@devsquad.com>
2022-06-30 18:18:46 -0600

    TS-10: groups: index backend component

--- app/Http/Livewire/Groups/Index.php
+++ app/Http/Livewire/Groups/Index.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Livewire\Groups;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class Index extends Component
 {
-    public function render()
+    public string $nome = 'Eduardo';
+
+    public function render(): Factory|View|Application
     {
         return view('livewire.groups.index');
+    }
+
+    public function getGroupsProperty()
+    {
+        return auth()->user()->groups;
     }
 }
--- tests/Feature/Livewire/Groups/IndexTest.php
+++ tests/Feature/Livewire/Groups/IndexTest.php
@@ -1,20 +1,35 @@
 <?php
 
-namespace Tests\Feature\Livewire\Groups;
+use App\Http\Livewire\Groups;
+use App\Models\Group;
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Livewire\livewire;
 
-use App\Http\Livewire\Groups\Index;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Livewire\Livewire;
-use Tests\TestCase;
+beforeEach(function () {
+    $this->user = User::factory()->createOne();
+    actingAs($this->user);
+});
 
-class IndexTest extends TestCase
-{
-    /** @test */
-    public function the_component_can_render()
-    {
-        $component = Livewire::test(Index::class);
+it('should list all the groups that I own', function () {
+    $groupsThatIOwn     = Group::factory()->count(3)->create(['user_id' => $this->user->id]);
+    $groupsThatIDontOwn = Group::factory()->count(3)->create(['user_id' => User::factory()
+        ->createOne()->id]);
 
-        $component->assertStatus(200);
-    }
-}
+    livewire(Groups\Index::class)
+        ->assertSet('groups', function ($groups) use ($groupsThatIDontOwn, $groupsThatIOwn) {
+            $iOwn = $groupsThatIOwn->whereIn('id', $groups->pluck('id'));
+
+            if ($iOwn->count() !== 3) {
+                return false;
+            }
+
+            $iDontOwn = $groupsThatIDontOwn->whereIn('id', $groups->pluck('id'));
+
+            if ($iDontOwn->count() === 3) {
+                return false;
+            }
+
+            return true;
+        });
+});
