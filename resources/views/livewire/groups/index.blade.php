229fb01ce16003cf340b28ec4942fc53caafccd6
Rafael Lunardelli <rafael@devsquad.com>
2022-09-29 18:07:36 -0600

    TS-13: updating group name

--- resources/views/livewire/groups/index.blade.php
+++ resources/views/livewire/groups/index.blade.php
@@ -3,22 +3,12 @@
 
     <x-container>
         @foreach($this->groups as $group)
-            <x-card>
-                <x-h.2 class=" flex justify-between items-center">
-                    <span>
-                        {{ $group->name }}
-                    </span>
-                    <span
-                        class="text-xs tracking-wider text-gray-800 bg-gray-50 px-2 rounded-lg">
-                        total points :: 20
-                    </span>
-                </x-h.2>
-                <div class="p-4 flex items-end space-x-2 justify-center">
-                    <div class="text-5xl font-bold">2</div>
-                    <div class="text-3xl -mb-1 text-gray-400">/</div>
-                    <div class="text-2xl -mb-2">20</div>
-                </div>
-            </x-card>
+            <div>
+                <livewire:groups.update
+                    wire:key="{{ now()->timestamp }}-group"
+                    :group="$group"
+                />
+            </div>
         @endforeach
 
         @unless($create)
